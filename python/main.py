bc369460915f9efdcf238d69478cf03aa7aacc48
Luiz Garcia <lldhgstar@gmail.com>
2026-08-05 17:23:36 -0300

    index 1

--- python/main.py
+++ python/main.py
@@ -1,2 +1,6 @@
-class chamado:
-    chamado = []
+class Chamado:
+   def __init__ (self,nome,setor,titulo,descricao,):
+       self.nome = nome
+       self.setor = setor
+       self.titulo = titulo
+       self.descricao = descricao
