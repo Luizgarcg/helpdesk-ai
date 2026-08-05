ac0a93c5a485807fa1b0e32d2900c50c2d2c6196
Luiz Garcia <lldhgstar@gmail.com>
2026-08-05 20:15:58 -0300

    index 14

--- python/servidor.py
+++ python/servidor.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 @app.route("/")
 def pagina_inicial():
     return render_template("index.html")
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    nome = request.form["nome"]
+    setor = request.form["setor"]
+    titulo = request.form["titulo"]
+    descricao = request.form["descricao"]
+    return f"Recebido: {nome}, {setor}, {titulo}, {descricao}"
 
 if __name__ == "__main__":
     app.run(debug=True)
