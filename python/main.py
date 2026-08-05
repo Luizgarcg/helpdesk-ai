fcaa92121bc33d0b987ded7fac737d867e69efde
Luiz Garcia <lldhgstar@gmail.com>
2026-08-05 17:41:50 -0300

    index 02

--- python/main.py
+++ python/main.py
@@ -1,6 +1,10 @@
+chamados = []
 class Chamado:
    def __init__ (self,nome,setor,titulo,descricao,):
        self.nome = nome
        self.setor = setor
        self.titulo = titulo
        self.descricao = descricao
+def cad_chamado(nome,setor,titulo,descricao):
+    novo = Chamado(nome,setor,titulo,descricao)
+    chamados.append(novo)
